503eb1184a4b1453accac30a40668dbcd57f58b7
anjali-gupta25 <72185129+anjali-gupta25@users.noreply.github.com>
2020-10-19 23:15:08 +0530

    Delete reverse_list.py

--- Python Codes Library/reverse_list.py
+++
@@ -1,9 +0,0 @@
-# Operating System List
-systems = ['Windows', 'macOS', 'Linux']
-print('Original List:', systems)
-
-# List Reverse
-systems.reverse()
-
-# updated list
-print('Updated List:', systems)
