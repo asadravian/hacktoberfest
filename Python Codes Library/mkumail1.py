60263e492d6891374c4e803128a9b8dcabe4f465
Aemonjabbar <49372579+Aemonjabbar@users.noreply.github.com>
2020-10-19 18:35:39 +0500

    Update mkumail1.py

--- Python Codes Library/mkumail1.py
+++ Python Codes Library/mkumail1.py
@@ -1,1 +1,17 @@
 print('Hello from Kumail')
+
+
+list1=[1,1.1,1+2j,'helloworld']
+print(list1)
+list2=[1,1.1,1+2j,'helloworld',[1,2,2,2,4]]
+print(list2)
+listnested=[[1,2],[3,4]]
+print(listnested)
+
+
+tup1=(1,1.1,1+2j,'helloworld')
+print(tup1)
+tup2=(1,1.2,1+2j,'helloworld',(1,2,2,2,2))
+print(tup2)
+tupnested=((1,2),(2,4))
+print(tupnested)
