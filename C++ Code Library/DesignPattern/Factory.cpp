b296acc099e37de51e05c1ca16927d6d0c6767e9
Aspects Pro <60023398+aspectspro@users.noreply.github.com>
2020-10-20 12:59:36 -0700

    Update Factory.cpp

--- C++ Code Library/DesignPattern/Factory.cpp
+++ C++ Code Library/DesignPattern/Factory.cpp
@@ -1,3 +1,45 @@
-class AbstractFactory{
+class Shape{
+public:
+    virtual void draw() = 0;
+};
 
+class Rectangle : public Shape{
+
+    // Shape interface
+public:
+    void draw() override
+    {
+        std::cout << "Drawing Rectangle";
+    }
+};
+
+class Circle : public Shape{
+
+    // Shape interface
+public:
+    void draw() override
+    {
+        std::cout << "Drawing Cirle";
+    }
+};
+
+class ShapeFactory{
+public:
+    static Shape * createShape(std::string type){
+        if(type == "RECTANGLE"){
+            return new Rectangle();
+        }
+        else if(type == "CIRCLE"{
+            return new Circle();
+        }
+        return new Rectangle();        
+    }
+};
+
+int main(){
+    Shape * rectangle = ShapeFactory::createShape("RECTANGLE");
+    Shape * circle = ShapeFactory::createShape("CIRCLE");
+
+    rectangle->draw();
+    circle->draw();
 }
