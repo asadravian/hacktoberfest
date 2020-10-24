a0009ef7d28bb4e7a68e7d710600b1cdb78a8ee6
Affan Ali <61421532+DevAffan@users.noreply.github.com>
2020-10-24 12:49:27 +0500

    Here i am contributing with switch statement code in c++

---
+++ C++ Code Library/Switching.cpp
@@ -0,0 +1,23 @@
+//here i am using Switch to check the valid condition  
+#include <iostream> 
+using namespace std; 
+
+int main() { 
+int x = 5; 
+	switch (x) 
+	{ 
+		case 1: 
+			cout << "Choice is 2"; 
+			break; 
+		case 2: 
+			cout << "Choice is 4"; 
+			break; 
+		case 3: 
+			cout << "Choice is 5"; 
+			break; 
+		default: 
+			cout << "Choice other than these cases then"; 
+			break; 
+	} 
+return 0; 
+} 
